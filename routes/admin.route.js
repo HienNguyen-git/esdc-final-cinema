0b1979d2d74d0fe1b25cef500fffc2272d9baa28
haidang2001 <nchdang16012001@gmail.com>
2022-04-27 21:54:29 +0700

    header admin

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -3,27 +3,27 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('admin/home', { title: 'Express' });
+  res.render('admin/home', { title: 'Express',path: "not-header",isAdmin: true });
 });
 
 router.get('/bookings', function(req, res, next) {
-  res.render('admin/bookings', { title: 'Express' });
+  res.render('admin/bookings', { title: 'Express',path: "not-header",isAdmin: true });
 });
 
 router.get('/movies', function(req, res, next) {
-  res.render('admin/movies', { title: 'Express' });
+  res.render('admin/movies', { title: 'Express',path: "not-header",isAdmin: true });
 });
 
 router.get('/hall', function(req, res, next) {
-  res.render('admin/hall', { title: 'Express' });
+  res.render('admin/hall', { title: 'Express',path: "not-header",isAdmin: true });
 });
 
 router.get('/employee', function(req, res, next) {
-  res.render('admin/employee', { title: 'Express' });
+  res.render('admin/employee', { title: 'Express',path: "not-header",isAdmin: true });
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('admin/login', { title: 'Express' });
+  res.render('admin/login', { title: 'Express',path: "not-header",isAdmin: true });
 });
 
 module.exports = router;
