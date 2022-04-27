788fe9ba00aab28b52ec50e61b35d30fa55f450a
HienNguyen-git <nguyentronghien.contact@gmail.com>
2022-04-27 22:19:09 +0700

    Revert "Something"
    This reverts commit bc9436b8b2abe910862acb5be2d4239975b1f5dd.

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -3,27 +3,27 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('admin/home', { title: 'Express',path: "not-header",isAdmin: true });
+  res.render('admin/home', { title: 'Express' });
 });
 
 router.get('/bookings', function(req, res, next) {
-  res.render('admin/bookings', { title: 'Express',path: "not-header",isAdmin: true });
+  res.render('admin/bookings', { title: 'Express' });
 });
 
 router.get('/movies', function(req, res, next) {
-  res.render('admin/movies', { title: 'Express',path: "not-header",isAdmin: true });
+  res.render('admin/movies', { title: 'Express' });
 });
 
 router.get('/hall', function(req, res, next) {
-  res.render('admin/hall', { title: 'Express',path: "not-header",isAdmin: true });
+  res.render('admin/hall', { title: 'Express' });
 });
 
 router.get('/employee', function(req, res, next) {
-  res.render('admin/employee', { title: 'Express',path: "not-header",isAdmin: true });
+  res.render('admin/employee', { title: 'Express' });
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('admin/login', { title: 'Express',path: "not-header",isAdmin: true });
+  res.render('admin/login', { title: 'Express' });
 });
 
 module.exports = router;
